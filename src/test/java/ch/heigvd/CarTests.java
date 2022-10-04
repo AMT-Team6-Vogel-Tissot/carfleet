5af2464c7628e9153ff0cc65c59ec1bbbb38ab0c
maelle <maelle.vogel@heig-vd.ch>
2022-10-04 17:55:49 +0200

    Ajout de deux tests par classe

--- src/test/java/ch/heigvd/CarTests.java
+++ src/test/java/ch/heigvd/CarTests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -15,7 +17,7 @@ import com.google.gson.stream.JsonReader;
 public class CarTests {
 
     private DriversToCar fleet = new DriversToCar();
-    private String jsonTest = "";
+
 
     @BeforeEach
     void doBeforeEach() throws FileNotFoundException {
@@ -29,14 +31,27 @@ public class CarTests {
 
     @Test
     void plateMustBePresent(){
-        String plateExcepted;
-        fleet.parseJsonToCar();
-        List<Car> cars = fleet.getCars();
+        String plateExcepted = "GE 123201";
+        Car carReceived = fleet.parseJsonToCar();
 
-        //recupere la voiture
-        //comparer les champs
-
+        assertEquals(plateExcepted,carReceived.getPlate());
 
 
     }
+
+    @Test
+    void mapMustBeTheSame(){
+        Map<String,String> mapExcepted = new HashMap<String, String>();
+        mapExcepted.put("Modèle","Volkswagen California");
+        mapExcepted.put("Sous-éléments","Coordonnées du chauffeur");
+        mapExcepted.put("Nom/prénom","Schmidt Alain");
+
+        Car carReceived = fleet.parseJsonToCar();
+
+        assertEquals(mapExcepted,carReceived.getColumnValues());
+
+
+    }
+
+
 }
--- src/test/java/ch/heigvd/DriverTests.java
+++ src/test/java/ch/heigvd/DriverTests.java
@@ -1,10 +1,53 @@
 package ch.heigvd;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DriverTests {
+
+    private DriversToCar fleet = new DriversToCar();
+    @BeforeEach
+    void doBeforeEach() throws FileNotFoundException {
+        String url = "utils\\dataDriver.json";
+        JsonReader reader = new JsonReader(new InputStreamReader(
+                new FileInputStream(url)));
+        JsonParser jsonParser = new JsonParser();
+        JsonObject userArray = jsonParser.parse(reader).getAsJsonObject();
+        System.out.println(userArray);
+    }
     @Test
     void plateMustBePresent(){
+        String plateExcepted = "GE 4567889";
+        Driver driverReceived = (Driver) fleet.parseJsonToDriver();
+
+        assertEquals(plateExcepted,driverReceived.getPlate());
+    }
+
+
+
+    @Test
+    void mapMustBeTheSame(){
+        Map<String,String> mapExcepted = new HashMap<String, String>();
+        mapExcepted.put("Nom/prénom","Libre Service");
+        mapExcepted.put("Téléphone","41276519164");
+        mapExcepted.put("E-mail","m.fontainesd@lift.ch");
+
+
+        Driver driverReceived = (Driver) fleet.parseJsonToDriver();
+
+        assertEquals(mapExcepted,driverReceived.getColumnValues());
+
 
     }
 }
