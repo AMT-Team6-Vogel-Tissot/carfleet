f01a01cc7d88b211500a7c0e39617ea572a41349
retroock <olivier.tissot21@gmail.com>
2022-10-04 17:41:37 +0200

    Ajout de la lecture du json pour les tests et ajout des fichiers .json

--- src/test/java/ch/heigvd/CarTests.java
+++ src/test/java/ch/heigvd/CarTests.java
@@ -1,15 +1,32 @@
 package ch.heigvd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.*;
 import java.util.List;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 
 public class CarTests {
 
     private DriversToCar fleet = new DriversToCar();
     private String jsonTest = "";
+
+    @BeforeEach
+    void doBeforeEach() throws FileNotFoundException {
+        String url = "utils\\dataCar.json";
+        JsonReader reader = new JsonReader(new InputStreamReader(
+                new FileInputStream(url)));
+        JsonParser jsonParser = new JsonParser();
+        JsonObject userArray = jsonParser.parse(reader).getAsJsonObject();
+        System.out.println(userArray);
+    }
+
     @Test
     void plateMustBePresent(){
         String plateExcepted;
