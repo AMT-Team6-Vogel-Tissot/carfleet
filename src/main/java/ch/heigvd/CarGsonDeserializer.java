f3c15fa842ed7e727a9deccbabd511235f3b0f6d
retroock <olivier.tissot21@gmail.com>
2022-10-16 00:52:52 +0200

    Step7 : tous les tests passent.

--- src/main/java/ch/heigvd/CarGsonDeserializer.java
+++ src/main/java/ch/heigvd/CarGsonDeserializer.java
@@ -13,15 +13,30 @@ public class CarGsonDeserializer implements JsonDeserializer<Car> {
 
         JsonObject jsonObject = json.getAsJsonObject();
 
+        if(jsonObject.size() == 0){
+            throw new JsonParseException("Json file is empty !");
+        }
+
+        if(!jsonObject.has("data")){
+            throw new JsonParseException("Bad structure");
+        }
+
         JsonObject items = jsonObject.get("data").getAsJsonObject()
                                      .get("boards").getAsJsonArray()
                                      .get(0).getAsJsonObject()
                                      .get("items").getAsJsonArray()
                                      .get(0).getAsJsonObject();
 
+        if(!items.has("id") || !items.has("name")){
+            throw new JsonParseException("one of the required field is missing !");
+        }
+
+        if(!items.has("column_values")){
+            throw new JsonParseException("column_values is missing !");
+        }
+
         JsonElement id = items.get("id");
         JsonElement licensePlate = items.get("name");
-
         JsonArray column_values = items.get("column_values").getAsJsonArray();
 
         ArrayList<Values> cv = new ArrayList<>();
--- src/main/java/ch/heigvd/DriverGsonDeserializer.java
+++ src/main/java/ch/heigvd/DriverGsonDeserializer.java
@@ -13,23 +13,38 @@ public class DriverGsonDeserializer implements JsonDeserializer<Driver> {
 
         JsonObject jsonObject = json.getAsJsonObject();
 
+        if(jsonObject.size() == 0){
+            throw new JsonParseException("Json file is empty !");
+        }
+
+        if(!jsonObject.has("data")){
+            throw new JsonParseException("Bad structure");
+        }
+
         JsonObject items = jsonObject.get("data").getAsJsonObject()
                                      .get("boards").getAsJsonArray()
                                      .get(0).getAsJsonObject()
                                      .get("items").getAsJsonArray()
                                      .get(0).getAsJsonObject();
 
-        JsonElement id = items.get("id");
-        JsonElement licensePlate = items.get("name");
         JsonObject subItems = items.get("subitems").getAsJsonArray()
                                    .get(0).getAsJsonObject();
 
-        JsonElement responsable = subItems.get("name");
+        if(!items.has("id") || !items.has("name") || !subItems.has("name")){
+            throw new JsonParseException("one of the required field is missing !");
+        }
+
+        if(!subItems.has("column_values")){
+            throw new JsonParseException("column_values is missing !");
+        }
 
         JsonArray column_values = subItems.get("column_values").getAsJsonArray();
 
-        ArrayList<Values> cv = new ArrayList<>();
+        JsonElement id = items.get("id");
+        JsonElement licensePlate = items.get("name");
+        JsonElement responsable = subItems.get("name");
 
+        ArrayList<Values> cv = new ArrayList<>();
         for (JsonElement j : column_values) {
             cv.add(new Values(j.getAsJsonObject().get("title").getAsString(), j.getAsJsonObject().get("text").getAsString()));
         }
